54e0c7f9177440137998b57d04fb67ed518c116f
Evedg <kapr2181@gmail.com>
2023-06-29 19:47:36 +0300

    избавился от неисправностей и доделал класс MAPluggableAccessor

--- MADictAccessor/MAPluggableAccessor_class.py
+++ MADictAccessor/MAPluggableAccessor_class.py
@@ -3,61 +3,44 @@ from MAAccessor_class import MAAccessor
 
 class MAPluggableAccessor(MAAccessor):
 
-    def __init__(self, aReadBlock, aWriteBlock):
-        self._readBlock = lambda aReadBlock: aReadBlock
-        self._writeBlock = lambda aReadBlock, aWriteBlock: aReadBlock.setdefault('new', aWriteBlock)
+    def __init__(self, aReadFunc, aWriteFunc):
+        self._readFunc = aReadFunc
+        self._writeFunc = aWriteFunc
 
     @classmethod
     def isAbstract(cls):
         return False
 
     def canRead(self, aModel):
-        if (self._readBlock != None):
-            return self._readBlock
+        if (self._readFunc != None):
+            return self._readFunc
         else:
             return None
 
     def canWrite(self, aModel):
-        if (self._writeBlock != None):
-            return self._writeBlock
+        if (self._writeFunc != None):
+            return self._writeFunc
         else:
             return None
 
     @property
-    def readBlock(self):
-        return self._readBlock
+    def readFunc(self):
+        return self._readFunc
 
-    @readBlock.setter
-    def readBlock(self, aBlock):
-        self._readBlock = aBlock
+    @readFunc.setter
+    def readFunc(self, aFunc):
+        self._readFunc = aFunc
 
     @property
-    def writeBlock(self):
-        return self._writeBlock
+    def writeFunc(self):
+        return self._writeFunc
 
-    @writeBlock.setter
-    def writeBlock(self, aBlock):
-        self._writeBlock = aBlock
+    @writeFunc.setter
+    def writeFunc(self, aFunc):
+        self._writeFunc = aFunc
 
-    # def read(self, aModel):
-    #     return aModel.get(self.readBlock)
     def read(self, aModel):
-        return self._readBlock(aModel)
+        return self._readFunc(aModel)
 
     def write(self, aModel, anObject):
-        self._writeBlock(aModel, anObject)
-
-
-d = {1: 10, 2: 11, 3: 13}
-m = MAPluggableAccessor(d, 3)
-m.write(d, 4)
-print(m.read(d))
-print(m.writeBlock)
-# def multiply(num1):
-#     var = 10
-#     def inner(num2):
-#         return num1 * num2
-#     return inner
-#
-# mult = multiply(9)
-# print(mult(3))
+        self._writeFunc(aModel, anObject)
