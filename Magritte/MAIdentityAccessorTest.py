d45293c3a05498956a33e01aa9f4d991228a8932
Evedg <kapr2181@gmail.com>
2023-07-04 16:20:57 +0300

    исправление недочетов MAIdentityAccessor

--- Magritte/MAIdentityAccessorTest.py
+++ Magritte/MAIdentityAccessorTest.py
@@ -8,6 +8,12 @@ class MAIdentityAccessorTest(TestCase):
         inst = MAIdentityAccessor()
         self.assertEqual(inst.canRead(aModel), True)
 
+    def test_canWrite(self):
+        aModel = {1: 11, 2: 12, 3: 13}
+        inst = MAIdentityAccessor()
+
+        self.assertEqual(inst.canWrite(aModel), False)
+
     def test_read(self):
         aModel = {1: 10, 2: 11, 3: 12}
         inst = MAIdentityAccessor()
@@ -16,8 +22,7 @@ class MAIdentityAccessorTest(TestCase):
     def test_write(self):
         aModel = {1: 10, 2: 11, 3: 12}
         inst = MAIdentityAccessor()
-        with self.assertRaises(Exception):
-            inst.write(aModel, 3)
+        self.assertRaises(Exception, inst.write, aModel, 3)
 
     def test_isAbstract(self):
         self.assertEqual(MAIdentityAccessor.isAbstract(), False)
