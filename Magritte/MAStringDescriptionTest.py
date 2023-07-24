9c493316f89d4f3ea63c2808ffae33d7191af52d
Evedg <kapr2181@gmail.com>
2023-07-24 16:33:36 +0300

    init

---
+++ Magritte/MAStringDescriptionTest.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from Magritte.MAStringDescription_class import MAStringDescription
+
+
+class MAStringDescriptionTest(TestCase):
+
+    def setUp(self):
+        self.inst = MAStringDescription()
+
+    def test_isAbstract(self):
+        self.assertEqual(MAStringDescription.isAbstract(), False)
+
+    def test_Kind(self):
+        self.assertEqual(self.inst.kind, str)
+
+    def test_Label(self):
+        self.assertEqual(self.inst.label, 'str')
+
+    def test_isSortable(self):
+        self.assertEqual(self.inst.isSortable(), True)
