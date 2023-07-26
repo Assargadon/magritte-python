5163e9b6c7bd15ca8c0524e22b5b348967deadd4
Evedg <kapr2181@gmail.com>
2023-07-26 12:54:22 +0300

    тесты для MAPriorityContainer

--- Magritte/MAPriorityContainerDescriptionTest.py
+++ Magritte/MAPriorityContainerDescriptionTest.py
@@ -1,11 +1,11 @@
 from unittest import TestCase
-from Magritte.MAPriorityContainerDescription_class import MAPriorityContainerDescription
+from Magritte.MAPriorityContainer_class import MAPriorityContainer
 
 
-class MAPriorityContainerDescriptionTest(TestCase):
+class MAPriorityContainerTest(TestCase):
 
     def setUp(self):
-        self.inst1 = MAPriorityContainerDescription()
+        self.inst1 = MAPriorityContainer()
         self.inst1.setChildren([1, 2, 3])
 
     def test_append(self):
@@ -15,12 +15,6 @@ class MAPriorityContainerDescriptionTest(TestCase):
     def test_extend(self):
         self.inst1.extend([4, 5, 6])
         self.assertEqual(self.inst1.children, [1, 2, 3, 4, 5, 6])
-
-    def test_moveDown(self):
-        self.assertRaises(NotImplementedError, self.inst1.moveDown, 4)
-
-    def test_moveUp(self):
-        self.assertRaises(NotImplementedError, self.inst1.moveUp, 4)
 
     def test_resort(self):
         self.inst1.resort()
