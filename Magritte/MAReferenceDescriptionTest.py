61abc66e4cf65cfc044596d64fd6c8706b7e0b7e
Evedg <kapr2181@gmail.com>
2023-07-31 15:18:18 +0300

    тесты для reference и relation

---
+++ Magritte/MAReferenceDescriptionTest.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+from Magritte.MAReferenceDescription_class import MAReferenceDescription
+from Magritte.MAStringDescription_class import MAStringDescription
+
+
+class MAReferenceDescriptionTest(TestCase):
+
+    def setUp(self):
+        self.inst1 = MAReferenceDescription()
+
+    def test_copy(self):
+        copy_test = self.inst1.__copy__()
+        self.assertEqual(copy_test, self.inst1)
+
+    def test_defaultReference(self):
+        self.assertEqual(isinstance(MAReferenceDescription.defaultReference, MAStringDescription), True)
+
+    def test_getInitializer(self):
+        self.assertEqual(self.inst1.initializer, self.inst1)
+
+    def test_setInitializer(self):
+        self.inst1.initializer = 123
+        self.assertEqual(self.inst1.initializer, 123)
+
+    def test_getReference(self):
+        self.assertEqual(isinstance(self.inst1.reference, MAStringDescription), True)
+
+    def test_setReference(self):
+        self.inst1.reference = [1, 2, 3]
+        self.assertEqual(self.inst1.reference, [1, 2, 3])
