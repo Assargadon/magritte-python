1cdfddd1b01a2f098e211d9e00b7819505016280
Evedg <kapr2181@gmail.com>
2023-07-31 17:23:22 +0300

    исправления тестов

--- Magritte/MAReferenceDescriptionTest.py
+++ Magritte/MAReferenceDescriptionTest.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
-from Magritte.MAReferenceDescription_class import MAReferenceDescription
-from Magritte.MAStringDescription_class import MAStringDescription
+from MAReferenceDescription_class import MAReferenceDescription
+from MAStringDescription_class import MAStringDescription
 
 
 class MAReferenceDescriptionTest(TestCase):
@@ -12,19 +12,12 @@ class MAReferenceDescriptionTest(TestCase):
         copy_test = self.inst1.__copy__()
         self.assertEqual(copy_test, self.inst1)
 
-    def test_defaultReference(self):
-        self.assertEqual(isinstance(MAReferenceDescription.defaultReference, MAStringDescription), True)
-
-    def test_getInitializer(self):
+    def test_initializer(self):
         self.assertEqual(self.inst1.initializer, self.inst1)
-
-    def test_setInitializer(self):
         self.inst1.initializer = 123
         self.assertEqual(self.inst1.initializer, 123)
 
-    def test_getReference(self):
-        self.assertEqual(isinstance(self.inst1.reference, MAStringDescription), True)
-
-    def test_setReference(self):
+    def test_reference(self):
+        self.assertIsInstance(self.inst1.reference, MAStringDescription)
         self.inst1.reference = [1, 2, 3]
         self.assertEqual(self.inst1.reference, [1, 2, 3])
--- Magritte/MARelationDescriptionTest.py
+++ Magritte/MARelationDescriptionTest.py
@@ -1,10 +1,10 @@
 from unittest import TestCase
-from Magritte.MARelationDescription_class import MARelationDescription
-from Magritte.MAPriorityContainer_class import MAPriorityContainer
-from Magritte.MAAccessor_class import MAAccessor
-from Magritte.MADictAccessor_class import MADictAccessor
-from Magritte.MANullAccessor_class import MANullAccessor
-from Magritte.MAStringDescription_class import MAStringDescription
+from MARelationDescription_class import MARelationDescription
+from MAPriorityContainer_class import MAPriorityContainer
+from MAAccessor_class import MAAccessor
+from MADictAccessor_class import MADictAccessor
+from MANullAccessor_class import MANullAccessor
+from MAStringDescription_class import MAStringDescription
 
 
 class MARelationDescriptionTest(TestCase):
@@ -15,24 +15,15 @@ class MARelationDescriptionTest(TestCase):
     def test_copy(self):
         self.assertEqual(self.inst1.__copy__(), self.inst1)
 
-    def test_defaultClasses(self):
-        self.assertEqual(MARelationDescription.defaultClasses(), set())
-
-    def test_getClasses(self):
+    def test_classes(self):
         self.assertEqual(self.inst1.classes, set())
-
-    def test_setClasses(self):
         self.inst1.classes = [int, bool, str]
         self.assertEqual(self.inst1.classes, [int, bool, str])
 
-    def test_allClasses(self):
-        self.inst1.classes = [int, bool, str]
-        self.assertEqual(self.inst1.allClasses, [bool, int, str])
-
     def test_commonClass(self):
-        self.assertEqual(isinstance(self.inst1.commonClass(), MAPriorityContainer), True)
-        self.inst1.classes = [MAAccessor, MADictAccessor, MANullAccessor]
+        self.assertIsInstance(self.inst1.commonClass(), MAPriorityContainer)
+        self.inst1.classes = [MADictAccessor, MAAccessor, MANullAccessor]
         self.assertEqual(self.inst1.commonClass(), MAAccessor)
 
     def test_reference(self):
-        self.assertEqual(isinstance(self.inst1.reference, MAStringDescription), True)
+        self.assertIsInstance(self.inst1.reference(), MAStringDescription)
