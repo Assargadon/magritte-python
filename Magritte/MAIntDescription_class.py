67120db19093e36847ae396313835caebbe7af20
Evedg <kapr2181@gmail.com>
2023-08-02 15:11:25 +0300

    реализация int и float и тесты для них

---
+++ Magritte/MAIntDescription_class.py
@@ -0,0 +1,17 @@
+from MANumberDescription_class import MANumberDescription
+from sys import intern
+
+
+class MAIntDescription(MANumberDescription):
+
+    @classmethod
+    def isAbstract(cls):
+        return False
+
+    @classmethod
+    def defaultKind(cls):
+        return int
+
+    @property
+    def label(self):
+        return 'int'
