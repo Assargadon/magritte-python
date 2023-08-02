c8e3b9fb9ef1728bae524e7105effecbdbfbca8f
Evedg <kapr2181@gmail.com>
2023-08-02 15:24:26 +0300

    реализация date and time и тесты для него

---
+++ Magritte/MADateAndTimeDescription_class.py
@@ -0,0 +1,17 @@
+from MAMagnitudeDescription_class import MAMagnitudeDescription
+from datetime import datetime
+
+
+class MADateAndTimeDescription(MAMagnitudeDescription):
+
+    @classmethod
+    def defaultKind(cls):
+        return datetime
+
+    @classmethod
+    def isAbstract(cls):
+        return False
+
+    @property
+    def label(self):
+        return "Date and Time"
