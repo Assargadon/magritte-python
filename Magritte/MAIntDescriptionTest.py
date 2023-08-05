d45d1de6f3d07ef3f91895d9e29d451bad06dcca
Yuriy Mironenko <assargaddon@gmail.com>
2023-08-05 22:05:20 +0300

    better MAIntDescriptionTest

--- Magritte/MAIntDescriptionTest.py
+++ Magritte/MAIntDescriptionTest.py
@@ -1,14 +1,17 @@
 from unittest import TestCase
 from MAIntDescription_class import MAIntDescription
+import MANumberDescriptionTest
 
+class TestProperties_of_MAIntDescription(MANumberDescriptionTest.TestProperties_of_MANumberDescription):
+
+    def get_description_instance_to_test(self):
+        return MAIntDescription()
 
 class MAIntDescriptionTest(TestCase):
 
     def setUp(self):
-        self.inst1 = MAIntDescription()
+        self.desc = MAIntDescription()
 
-    def test_kind(self):
-        self.assertEqual(self.inst1.kind, int)
+    def test_kind_by_default(self):
+        self.assertEqual(self.desc.kind, int)
 
-    def test_label(self):
-        self.assertEqual(self.inst1.label, 'int')
