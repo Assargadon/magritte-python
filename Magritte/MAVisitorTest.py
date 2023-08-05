363ef72d9a2f5f801ca828a87c346bcad4b57c96
Yuriy Mironenko <assargaddon@gmail.com>
2023-08-05 19:56:13 +0300

    MAVisitorTest / testAll

--- Magritte/MAVisitorTest.py
+++ Magritte/MAVisitorTest.py
@@ -9,41 +9,6 @@ from MAReferenceDescription_class import MAReferenceDescription
 from MARelationDescription_class import MARelationDescription
 from MAStringDescription_class import MAStringDescription
 
-class TestMAVisitorInheritance(TestCase):
-
-
-    def get_inheritance_chain(self, cls):
-        return [base.__name__ for base in cls.__mro__ if base != object]
-
-    def check_descriptor(self, descriptor):
-        # Get expected inheritance chain
-        expected_chain = self.get_inheritance_chain(descriptor)
-
-        # Create a visitor and visit the description
-        visitor = MATestingVisitor()
-        description_instance = descriptor()
-        visitor.visit(description_instance)
-
-        # Assert
-        self.assertEqual(visitor.visited_methods, expected_chain, f"For {descriptor.__name__}, visitor did not correctly mimic inheritance chain")
-        # print(visitor.visited_methods)
-
-    def test_visitor_mimics_inheritance(self):
-        # Arrange
-        descriptors_to_test = [
-            MADescription,
-            MAElementDescription,
-            MAMagnitudeDescription,
-            MAOptionDescription,
-            MAReferenceDescription,
-            MARelationDescription,
-            MAStringDescription
-        ]  # Add other classes here
-
-        for descriptor in descriptors_to_test:
-            with self.subTest(descriptor=descriptor):
-                self.check_descriptor(descriptor)
-
 
 class MATestingVisitor(MAVisitor):
     def __init__(self):
@@ -76,3 +41,56 @@ class MATestingVisitor(MAVisitor):
     def visitStringDescription(self, anObject):
         self.visited_methods.append(MAStringDescription.__name__)
         super().visitStringDescription(anObject)
+
+
+class MATestingVisitAllVisitor(MAVisitor):
+        def __init__(self):
+            self.visited_descriptors = []
+
+        def visit(self, descriptor):
+            self.visited_descriptors.append(descriptor)
+
+
+class MAVisitorTest(TestCase):
+
+    descriptors_to_test = [
+        MADescription,
+        MAElementDescription,
+        MAMagnitudeDescription,
+        MAOptionDescription,
+        MAReferenceDescription,
+        MARelationDescription,
+        MAStringDescription
+    ]  # Add other classes here
+
+    def get_inheritance_chain(self, cls):
+        return [base.__name__ for base in cls.__mro__ if base != object]
+
+    def check_descriptor(self, descriptor):
+        # Get expected inheritance chain
+        expected_chain = self.get_inheritance_chain(descriptor)
+
+        # Create a visitor and visit the description
+        visitor = MATestingVisitor()
+        description_instance = descriptor()
+        visitor.visit(description_instance)
+
+        # Assert
+        self.assertEqual(visitor.visited_methods, expected_chain, f"For {descriptor.__name__}, visitor did not correctly mimic inheritance chain")
+        # print(visitor.visited_methods)
+
+    def test_visitor_mimics_inheritance(self):
+
+        for descriptor in self.descriptors_to_test:
+            with self.subTest(descriptor=descriptor):
+                self.check_descriptor(descriptor)
+
+    def test_visitAll(self):
+    
+        testing_visitor = MATestingVisitAllVisitor()
+        testing_visitor.visitAll(self.descriptors_to_test)
+
+        self.assertEqual(set(testing_visitor.visited_descriptors), set(self.descriptors_to_test),
+            "The visitor's visit method did not visit all the expected descriptors")
+    
+    
