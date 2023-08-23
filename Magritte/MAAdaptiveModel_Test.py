4b3c55a575eb61da29a5fffe378660083a245250
Yuriy Mironenko <assargaddon@gmail.com>
2023-08-23 15:51:23 +0300

    MAAdaptiveModelTest

---
+++ Magritte/MAAdaptiveModel_Test.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+from MAAdaptiveModel_class import MAAdaptiveModel
+from MAStringDescription_class import MAStringDescription
+from MANumberDescription_class import MANumberDescription
+
+
+class MAAdaptiveModelTest(TestCase):
+
+    def setUp(self):
+        self.nameDesc = MAStringDescription()
+        self.ageDesc = MANumberDescription()
+
+        self.adaptiveModel = MAAdaptiveModel()        
+        self.adaptiveModel.magritteDescription += nameDesc 
+        self.adaptiveModel.magritteDescription += ageDesc
+        
+    def test_write_read(self):
+        self.adaptiveModel.writeUsing(description = self.nameDesc, value = "goodok")
+        self.adaptiveModel.writeUsing(description = self.ageDesc, value = 40)
+        
+        self.assertEqual(self.adaptiveModel.readUsing(self.nameDesc), "goodok")
+        self.assertEqual(self.adaptiveModel.readUsing(self.ageDesc), 40)
+
+    def test_read_uninitialized(self):
+        self.assertIsNone(self.adaptiveModel.readUsing(self.nameDesc))
+        self.assertIsNone(self.adaptiveModel.readUsing(self.ageDesc))
+
+    def test_access_undescribed(self):
+        notFieldOfAdaptiveModel = MAStringDescription("not_a_field")
+
+        with self.assertRaises(MAReadError):
+            self.adaptiveModel.readUsing(notFieldOfAdaptiveModel)
+
+        with self.assertRaises(MAWriteError):
+            self.adaptiveModel.writeUsing(notFieldOfAdaptiveModel, "some value")
