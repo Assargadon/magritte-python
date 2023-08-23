733ee00fb9a2cf9f7c64fe27c13c8523911df2ab
Константин Калинин <konstantin.kalinin@wizn.tech>
2023-08-23 13:40:36 +0300

    MAAdaptiveModel

--- Magritte/MAAdaptiveModel_class.py
+++ Magritte/MAAdaptiveModel_class.py
@@ -1,6 +1,24 @@
 
+from MAContainer_class import MAContainer
+
 
 class MAAdaptiveModel:
 
-    # TODO
-    pass
+    def __init__(self):
+        self.magritteDescription = self.defaultDescription()
+        self.values = self.defaultDictionary()
+
+    @classmethod
+    def defaultDescription(cls):
+        return MAContainer()
+
+    @classmethod
+    def defaultDictionary(cls):
+        return dict()
+
+    def readUsing(self, aDescription):
+        return self.values[aDescription] if aDescription in self.values else None
+
+    def writeUsing(self, anObject, aDescription):
+        self.values[aDescription] = anObject
+
