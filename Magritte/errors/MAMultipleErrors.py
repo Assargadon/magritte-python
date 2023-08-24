ec8082a2201d487362950c0cfc745639444c3562
Evedg <kapr2181@gmail.com>
2023-08-24 16:50:38 +0300

    MAError и его потомки
    # Conflicts:
    #	Magritte/errors/MAError.py
    #	Magritte/errors/MAMultipleErrors.py
    #	Magritte/errors/MAValidationError.py

--- Magritte/errors/MAMultipleErrors.py
+++ Magritte/errors/MAMultipleErrors.py
@@ -4,4 +4,8 @@ from MAValidationError import MAValidationError
 class MAMultipleErrors(MAValidationError):
     def __init__(self, aDescription, aCollection, aString):
         super().__init__(aDescription, aString)
-    
+
+        self._collection = aCollection
+
+    def setCollection(self, aCollection):
+        self._collection = aCollection
--- Magritte/errors/MAValidationError.py
+++ Magritte/errors/MAValidationError.py
@@ -3,4 +3,12 @@ from MAError import MAError
 
 class MAValidationError(MAError):
     def __init__(self, aDescription, aString):
-        pass
+        self._description = aDescription
+        self._string = aString
+
+    @classmethod
+    def isResumable(cls):
+        return True
+
+    def setDescription(self, aDescription):
+        self._description = aDescription
