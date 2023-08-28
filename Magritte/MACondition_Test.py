4d895ccfd3e0c087aca875946f3063cecb51f4c5
Константин Калинин <konstantin.kalinin@wizn.tech>
2023-08-28 15:53:39 +0300

    Метод проверки MACondition называется почему-то value(), а не test()

--- Magritte/MACondition_Test.py
+++ Magritte/MACondition_Test.py
@@ -6,17 +6,17 @@ class MAConditionTest(TestCase):
 
     def test_selector(self):
         condition = MACondition.selector("is_integer") #такой метод есть у float
-        self.assertTrue(condition.test(-1.0))
-        self.assertFalse(condition.test(0.5))
+        self.assertTrue(condition.value(-1.0))
+        self.assertFalse(condition.value(0.5))
 
 
     def test_selectorArgument(self):
         condition = MACondition.selectorArgument("__lt__", 5)
-        self.assertTrue(condition.test(1))
-        self.assertFalse(condition.test(5))
+        self.assertTrue(condition.value(1))
+        self.assertFalse(condition.value(5))
         
     def test_receiverSelector(self):
         condition = MACondition.selectorArgument(1, "__eq__")
-        self.assertTrue(condition.test(1))
-        self.assertFalse(condition.test(0))
+        self.assertTrue(condition.value(1))
+        self.assertFalse(condition.value(0))
         
