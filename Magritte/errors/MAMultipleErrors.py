59e80963e36845e7720a378a9c3204d2cf666722
Константин Калинин <konstantin.kalinin@wizn.tech>
2023-08-31 13:17:21 +0300

    Относительные пути импорта в errors/* (без этого юниттесты в pycharm не могут импортировать эти классы)

--- Magritte/errors/MAMultipleErrors.py
+++ Magritte/errors/MAMultipleErrors.py
@@ -1,7 +1,11 @@
-from MAValidationError import MAValidationError
+from . MAValidationError import MAValidationError
 
 
 class MAMultipleErrors(MAValidationError):
     def __init__(self, aDescription, errors, message):
         super().__init__(aDescription, message)
         self.collection = errors
+
+    def __str__(self):
+        stringArray = [str(element) for element in self.collection]
+        return '\n'.join(stringArray)
