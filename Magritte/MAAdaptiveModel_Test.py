22ab58212a2f49ec0a8e2dabe367bbb38ccd0945
Константин Калинин <konstantin.kalinin@wizn.tech>
2023-09-07 17:44:45 +0300

    Второе поле того же типа MAStringDescription в тесте, для проверки на различимость

--- Magritte/MAAdaptiveModel_Test.py
+++ Magritte/MAAdaptiveModel_Test.py
@@ -10,21 +10,26 @@ class MAAdaptiveModelTest(TestCase):
 
     def setUp(self):
         self.nameDesc = MAStringDescription()
+        self.surnameDesc = MAStringDescription()
         self.ageDesc = MANumberDescription()
 
         self.adaptiveModel = MAAdaptiveModel()        
         self.adaptiveModel.magritteDescription += self.nameDesc 
+        self.adaptiveModel.magritteDescription += self.surnameDesc
         self.adaptiveModel.magritteDescription += self.ageDesc
         
     def test_write_read(self):
         self.adaptiveModel.writeUsing(description = self.nameDesc, value = "goodok")
+        self.adaptiveModel.writeUsing(description = self.surnameDesc, value = "assargadonov")
         self.adaptiveModel.writeUsing(description = self.ageDesc, value = 40)
         
         self.assertEqual(self.adaptiveModel.readUsing(self.nameDesc), "goodok")
+        self.assertEqual(self.adaptiveModel.readUsing(self.surnameDesc), "assargadonov")
         self.assertEqual(self.adaptiveModel.readUsing(self.ageDesc), 40)
 
     def test_read_uninitialized(self):
         self.assertIsNone(self.adaptiveModel.readUsing(self.nameDesc))
+        self.assertIsNone(self.adaptiveModel.readUsing(self.surnameDesc))
         self.assertIsNone(self.adaptiveModel.readUsing(self.ageDesc))
 
     def test_access_undescribed(self):
