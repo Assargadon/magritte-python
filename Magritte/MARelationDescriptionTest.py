fe8bb7988f986db16692d4522484c64ec1de7f6c
Yuriy Mironenko <assargaddon@gmail.com>
2023-09-08 17:50:19 +0300

    `reference` is excluded from standard properties tests - it's not nullable anymore
    I.e. you cannot set reference to None - if so, it tries to build reference automatically

--- Magritte/MARelationDescriptionTest.py
+++ Magritte/MARelationDescriptionTest.py
@@ -15,12 +15,14 @@ class TestProperties_of_MARelationDescription(MAReferenceDescriptionTest.TestPro
 
     def _properties(self):
         #{**dict1, **dict2} is actually a merge of two dictionaries
-        return {
+        myProps = {
             **super()._properties(),
             **{
                 'classes': set
             }
         }
+        myProps.pop("reference") #reference is no None-accepting in sthis class and subclasses
+        return myProps 
 
 
 class MARelationDescriptionTest(TestCase):
