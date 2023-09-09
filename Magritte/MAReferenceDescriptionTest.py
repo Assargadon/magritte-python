c8e1e4434b0d4d798df60fa4dda065998946f421
Yuriy Mironenko <assargaddon@gmail.com>
2023-09-09 11:01:45 +0300

    `TestProperties_of_MAReferenceDescription` почему-то был унаследован от `TestProperties_of_MAStringDescription` - исправлено

--- Magritte/MAReferenceDescriptionTest.py
+++ Magritte/MAReferenceDescriptionTest.py
@@ -2,10 +2,10 @@ from unittest import TestCase
 from MAReferenceDescription_class import MAReferenceDescription
 from MAStringDescription_class import MAStringDescription
 from MADescription_class import MADescription
-import MAStringDescriptionTest
+import MAElementDescriptionTest
 
 
-class TestProperties_of_MAReferenceDescription(MAStringDescriptionTest.TestProperties_of_MAStringDescription):
+class TestProperties_of_MAReferenceDescription(MAElementDescriptionTest.TestProperties_of_MAElementDescription):
     def get_description_instance_to_test(self):
         return MAReferenceDescription()
 
