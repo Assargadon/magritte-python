03028dc2e26e842769d46d4649c5f93e18932e30
Yuriy Mironenko <assargaddon@gmail.com>
2023-09-14 16:07:00 +0300

    readonly, visible, required

--- Magritte/MADescription_selfdesc.py
+++ Magritte/MADescription_selfdesc.py
@@ -1,6 +1,8 @@
 from MAPriorityContainer_class import MAPriorityContainer
 from MAStringDescription_class import MAStringDescription
 from MAIntDescription_class import MAIntDescription
+from MABooleanDescription_class import MABooleanDescription
+
 from accessors.MAAttrAccessor_class import MAAttrAccessor
 
 def magritteDescription(self):
@@ -8,5 +10,8 @@ def magritteDescription(self):
     
     desc += MAStringDescription(label = "Label", priority = 100, default = "#nolabel#", required = True, accessor = MAAttrAccessor('label'))
     desc += MAIntDescription(label = "Priority", priority = 130, default = self.defaultPriority(), required = True, accessor = MAAttrAccessor('priority'))
-
+    desc += MABooleanDescription(label = 'Read-only', priority = 200, default = self.defaultReadOnly(), accessor = MAAttrAccessor('readOnly'))
+    desc += MABooleanDescription(label = 'Visible', priority = 210, default = self.defaultVisible(), accessor = MAAttrAccessor('visible'))
+    desc += MABooleanDescription(label = 'Required', priority = 220, default = self.defaultRequired(), accessor = MAAttrAccessor('required'))
     return desc
+    
