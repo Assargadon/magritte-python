72dba1cbe795a5a4ac77e2681e97cbc176215b30
Yuriy Mironenko <assargaddon@gmail.com>
2023-09-14 16:17:37 +0300

    name, group, comment

--- Magritte/MADescription_selfdesc.py
+++ Magritte/MADescription_selfdesc.py
@@ -8,7 +8,10 @@ from accessors.MAAttrAccessor_class import MAAttrAccessor
 def magritteDescription(self):
     desc = MAPriorityContainer()
     
+    desc += MAStringDescription(priority = 1, accessor = MAAttrAccessor('name'))
     desc += MAStringDescription(label = "Label", priority = 100, default = "#nolabel#", required = True, accessor = MAAttrAccessor('label'))
+    desc += MAStringDescription(label = "Group", priority = 105, default = self.defaultGroup(), accessor = MAAttrAccessor('group'))
+    desc += MAStringDescription(label = "Comment", priority = 110, default = self.defaultComment(), accessor = MAAttrAccessor('comment'))
     desc += MAIntDescription(label = "Priority", priority = 130, default = self.defaultPriority(), required = True, accessor = MAAttrAccessor('priority'))
     desc += MABooleanDescription(label = 'Read-only', priority = 200, default = self.defaultReadOnly(), accessor = MAAttrAccessor('readOnly'))
     desc += MABooleanDescription(label = 'Visible', priority = 210, default = self.defaultVisible(), accessor = MAAttrAccessor('visible'))
