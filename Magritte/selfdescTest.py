5753cfe2090f7db9caf2b189e8e5a1a492d0ee12
Yuriy Mironenko <assargaddon@gmail.com>
2023-09-15 11:43:30 +0300

    better selfdesc test printout
    works with non-json values as accessors

--- Magritte/selfdescTest.py
+++ Magritte/selfdescTest.py
@@ -1,18 +1,70 @@
 from unittest import TestCase
 import json
-
+from datetime import datetime
+        
 from MAContainer_class import MAContainer
 from MAStringDescription_class import MAStringDescription
 from MAIntDescription_class import MAIntDescription
 from MAFloatDescription_class import MAFloatDescription
 from MADateAndTimeDescription_class import MADateAndTimeDescription
 
+from MAVisitor_class import MAVisitor
+
+class TestVisualizerVisitor(MAVisitor):
+    
+    def convert(self, model, description = None):
+        if not description: description = model.magritteDescription()
+
+        self.json = None
+        self.model = model
+        
+        self.visit(description)
+        
+        return self.json
+        
+    def deeper(self, model, description = None):
+        prev_json = self.json
+        prev_model = self.model
+        
+        res = self.convert(model, description)
+
+        self.json = prev_json
+        self.model = prev_model
+        
+        return res
+
+    def visitContainer(self, description):
+        if not self.json:
+            self.json = {}
+            self.visitAll(description)
+        else:
+            raise Exception("Shouldn't reach visitContainer with nonempty self.json")
+
+    def visitElementDescription(self, description):
+        value = description.accessor.read(self.model)
+        
+        try:
+            json.dumps(value) #for test if value is json-serializable
+            self.json[description.name] = value #if so, then just put raw value
+        except:
+            self.json[description.name] = f"!{value}!"
+
+    def visitToOneRelationDescription(self, description):
+        related_object = description.accessor.read(self.model)
+        if(not description.reference):
+            self.visitRelationDescription(description)
+        else:
+            self.json[description.name] = self.deeper(related_object, description.reference)
+
+    def visitToManyRelationDescription(self, description):
+        collection = description.accessor.read(self.model) # TODO: replace on model.readUsing or description.read (both are not implemented yet)
+        self.json[description.name] = [self.deeper(entry, description.reference) for entry in collection]
 
 class MagritteSelfDescriptionTest(TestCase):
 
     def test_magritteDescription(self):
-        from datetime import datetime
-        from MAJsonWriter_visitors import MAValueJsonWriter, MAObjectJsonWriter
+#        from datetime import datetime
+#        from MAJsonWriter_visitors import MAValueJsonWriter, MAObjectJsonWriter
 
         object_desc = MAContainer()
         object_desc.label = "Demo Object"
@@ -21,7 +73,7 @@ class MagritteSelfDescriptionTest(TestCase):
         object_desc += MAFloatDescription(name='float_value', label='Float Value', default=0.0)
         object_desc += MADateAndTimeDescription(name='date_value', label='Date Value', default=datetime.now())
 
-        object_encoder = MAObjectJsonWriter(object_desc.magritteDescription())
-        metadescriptor_json = object_encoder.write_json(object_desc)
+        object_encoder = TestVisualizerVisitor()
+        metadescriptor_json = object_encoder.convert(object_desc)
         
         print(f"description's description (in JSON):\n{json.dumps(metadescriptor_json, indent=4)}")
