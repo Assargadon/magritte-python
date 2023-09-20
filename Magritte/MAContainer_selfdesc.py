9bef392fc9ca1ea10ac207546af43f0910c811ae
Yuriy Mironenko <assargaddon@gmail.com>
2023-09-20 16:59:01 +0300

    MAContainer.children code restructuring
    previous approach was needed to overcome reference "init overwriting" bug, which was fixes

--- Magritte/MAContainer_selfdesc.py
+++ Magritte/MAContainer_selfdesc.py
@@ -5,16 +5,14 @@ from MAElementDescription_class import MAElementDescription
 def magritteDescription(self, parentDescription):
     desc = parentDescription
     
-    children = MAToManyRelationDescription(
+    desc += MAToManyRelationDescription(
         name="children",
         label="Elements",
         priority=400,
         default=self.defaultCollection(),
         classes=[MAElementDescription],
-        #reference = MAElementDescription().magritteDescription(),
+        reference = MAElementDescription().magritteDescription(),
         accessor = MAIdentityAccessor()
     )
-    children.reference = MAElementDescription().magritteDescription()
     
-    desc += children
     return desc
