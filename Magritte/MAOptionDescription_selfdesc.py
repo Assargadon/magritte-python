b4d6ef20742867a315b62f8985a535c19dde7318
Yuriy Mironenko <assargaddon@gmail.com>
2023-09-22 17:20:53 +0300

    typos

--- Magritte/MAOptionDescription_selfdesc.py
+++ Magritte/MAOptionDescription_selfdesc.py
@@ -9,7 +9,7 @@ def magritteDescription(self, parentDescription):
     
     desc += MABooleanDescription(label='Sorted', priority=240, default=self.defaultSorted(), accessor=intern('sorted'))
     desc += MABooleanDescription(label='Extensible', priority=250, default=self.defaultExtensible(), accessor=intern('extensible'))
-    desc += MAToManyRelationDescription(label = "Options", priority = 410, default = self.defaultOptions(), reference = self.reference, accessor = intern('options'))
+    desc += MAToManyRelationDescription(label="Options", priority=410, default=self.defaultOptions(), reference=self.reference, accessor=intern('options'))
 
     # ========= special ==========    
     desc += MAElementDescription(label = "Grouped By", priority = 260, accessor = intern('groupBy'), comment = "Accessor to retrieve value used for grouping of option objects out of these option objects")
