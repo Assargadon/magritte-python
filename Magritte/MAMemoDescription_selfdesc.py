838989d0816dd0e21b4da70e63bc63b952c79100
Evedg <kapr2181@gmail.com>
2023-09-27 15:06:05 +0300

    реализация метадескриптора

---
+++ Magritte/MAMemoDescription_selfdesc.py
@@ -0,0 +1,10 @@
+from sys import intern
+from MANumberDescription_class import MANumberDescription
+
+
+def magritteDescription(self, parentDescription):
+    desc = parentDescription
+
+    desc += MANumberDescription(label='Number of Lines', priority=400, default=self.defaultLineCount(), accessor=intern('lineCount'))
+
+    return desc
