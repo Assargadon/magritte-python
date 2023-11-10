d2118e0e88744630f8ca937c16fb0c4b98c47d08
Faris Faris <farisfarishunt@yandex.ru>
2023-11-10 20:28:56 +0300

    API fix

--- MagritteSQL/lib/MagritteSQL/db_deployer.py
+++ MagritteSQL/lib/MagritteSQL/db_deployer.py
@@ -1,46 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy import Engine
 from typing import Any
+from typing import Self
 from Magritte.MAContainer_class import MAContainer
 
 class DbDeployer:
 
-    def __init__(self):
-        self.engines = []
-        self.tables = []
+    def __init__(self, engine: Engine):
+        self.engine = engine
+        self.models: dict = {}
 
-    """
-    Adds new engine to engine list of the DbDeployer. Engine is basically a db connection
+    @classmethod
+    def create_db_deployer(self, connline: str, **kwargs: Any) -> Self:
+        engine = create_engine(connline, **kwargs)
+        return DbDeployer(engine)
 
-    :param self: instance object itself
-    :param str connline: connection line to connect db, would be passed directly to SQLAlchemy
-    :param Any kwargs: any additional engine options, would be passed directly to SQLAlchemy
-    :return: None
-    :raises Exception: any exception that SQLAlchemy raise
-    """
-    def add_engine(self, connline: str, **kwargs: Any) -> None:
-        self.engines.append(create_engine(connline, kwargs))
-
-    """
-    Adds new table to table list of the DbDeployer. The table is described with Magritte description
-
-    :param self: instance object itself
-    :param MAContainer magrittedescription: the table magritte description to be added
-    :return: None
-    :raises Exception: any exception that MAContainer members raise
-    :raises Exception: invalid magritte description
-    """
-    def add_table(self, magrittedescription: MAContainer) -> None:
+    def register_model(self, magrittedescription: MAContainer) -> None:
         pass
 
-    """
-    Deploys table list to all engines of the engine list
+    def deploy_table(self, model_name: str) -> None:
+        pass
 
-    :param self: instance object itself
-    :return: None
-    :raises Exception: any exception that SQLAlchemy raise
-    :raises Exception: no engines were specified
-    :raises Exception: no tables were specified
-    """
     def deploy_db(self) -> None:
         pass
--- MagritteSQL/test/test_db_deployer.py
+++ MagritteSQL/test/test_db_deployer.py
@@ -1,1 +1,14 @@
-pass
+from unittest import TestCase
+
+from lib.MagritteSQL.db_deployer import DbDeployer
+
+class DbDeployerTest(TestCase):
+
+    def test_deploy_table_unregistered_model(self):
+        try:
+            deployer = DbDeployer.create_db_deployer("sqlite://")
+            deployer.deploy_table("person")
+        except Exception as e:
+            print(e)
+            return
+        assert False
