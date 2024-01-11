37b36b3a15a302585da7b3651b991bcb44270b0d
Константин Калинин <konstantin.kalinin@wizn.tech>
2024-01-11 16:44:56 +0300

    Рефакторинг MAReferencedDataUntangler.
    Убран прямой предок MAVisitor, чтоб не было желания использовать этот объект как визитор в лоб.
    Добавлен флаг обхода, запрещающий/разрешающий считывание элементарных значений при обходе.

--- Magritte/visitors/MAReferencedDataWriterReader_visitors.py
+++ Magritte/visitors/MAReferencedDataWriterReader_visitors.py
@@ -8,146 +8,121 @@ from Magritte.MAModel_class import MAModel
 from Magritte.visitors.MAJsonWriter_visitors import MAValueJsonWriter
 
 
-class MAReferencedDataUntangler(MAVisitor):
+class MAReferencedDataUntangler:
 
-    class _Context:
-        context_index: int = None 
-        model: Any = None
-        description: MADescription = None
-        value_index: int = None
-        subcontexts: list = None
-        _dbg_ref_count: int = 1
-        _dbg_value: None
+    class _Walker(MAVisitor):
 
-    #class _Reference:
-    #    value: Any = None
-    #    reference_index: int = 0
+        class _Context:
+            context_index: int = None
+            model: Any = None
+            description: MADescription = None
+            value_index: int = None
+            subcontexts: list = None
+            _dbg_ref_count: int = 1
+            _dbg_value: None
 
-    def __init__(self):
-        self._clear()
+        def __init__(self):
+            self._clear()
 
-    def _clear(self):
-        self._values = []
-        self._values_by_value_index = {}
-        self._value_indices_by_identifier = {}
-        self._contexts = []
-        self._contexts_by_value_index = {}
-        self._createEmptyContext()
+        def _clear(self):
+            self._doReadElementValues = False
+            self._values = []
+            self._values_by_value_index = {}
+            self._value_indices_by_identifier = {}
+            self._contexts = []
+            self._contexts_by_value_index = {}
+            self._createEmptyContext()
 
-    def _dbg_print(self):
-        printed_contexts = set()
-        def printContext(sPrefix: str, aContext: MAReferencedDataUntangler._Context):
-            context_index = aContext.context_index
-            printed_contexts.add(context_index)
-            print(f'{sPrefix}Context {context_index}, {aContext.description.name}, referenced {aContext._dbg_ref_count} time(s):')
-            subPrefix = f'{sPrefix}  '
-            if aContext.value_index is None:
-                for subcontext in aContext.subcontexts:
-                    subcontext_index = subcontext.context_index
-                    if subcontext_index in printed_contexts:
-                        print(f'{subPrefix}Context {subcontext_index}, {subcontext.description.name} // already printed')
-                    else:
-                        printContext(subPrefix, subcontext)
-            else:
-                print(f'{subPrefix}Value {aContext.value_index}, {self._values[aContext.value_index]}')
+        def _dbg_print(self):
+            printed_contexts = set()
+            def printContext(sPrefix: str, aContext):
+                context_index = aContext.context_index
+                printed_contexts.add(context_index)
+                print(f'{sPrefix}Context {context_index}, {aContext.description.name}, referenced {aContext._dbg_ref_count} time(s):')
+                subPrefix = f'{sPrefix}  '
+                if aContext.value_index is None:
+                    for subcontext in aContext.subcontexts:
+                        subcontext_index = subcontext.context_index
+                        if subcontext_index in printed_contexts:
+                            print(f'{subPrefix}Context {subcontext_index}, {subcontext.description.name} // already printed')
+                        else:
+                            printContext(subPrefix, subcontext)
+                else:
+                    print(f'{subPrefix}Value {aContext.value_index}, {self._values[aContext.value_index]}')
 
-        printContext('', self._contexts[0])
+            printContext('', self._contexts[0])
 
-    def _createEmptyContext(self):
-        context_index = len(self._contexts)
-        self._context = self.__class__._Context()
-        self._contexts.append(self._context)
-        self._context.context_index = context_index
-        return self._context
+        def _createEmptyContext(self):
+            context_index = len(self._contexts)
+            self._context = self.__class__._Context()
+            self._contexts.append(self._context)
+            self._context.context_index = context_index
+            return self._context
 
-    #def _pushContext(self):
-    #    self._contexts.append(self._context)
+        #def _pushContext(self):
+        #    self._contexts.append(self._context)
 
-    #def _popContext(self):
-    #    self._context = self._contexts.pop()
+        #def _popContext(self):
+        #    self._context = self._contexts.pop()
 
-    def _addValue(self, aValue):
-        value_index = len(self._values)
-        self._values.append(aValue)
-        self._values_by_value_index[value_index] = aValue
-        return value_index
-
-    def _addValueWithCheck(self, aValue):
-        identifier = id(aValue)
-        if identifier in self._value_indices_by_identifier:
-            was_added = False
-            value_index = self._value_indices_by_identifier[identifier]
-        else:
-            was_added = True
+        def _addValue(self, aValue):
             value_index = len(self._values)
             self._values.append(aValue)
-            #reference = self.__class__._Reference()
-            #reference.reference_index = reference_index
-            #reference.value = aValue
-            self._value_indices_by_identifier[identifier] = value_index
             self._values_by_value_index[value_index] = aValue
-        return (value_index, was_added,)
+            return value_index
 
-    def processModel(self, aModel: Any, aDescription: MADescription):
-        self._clear()
-        self._context.model = aModel
-        self._context.description = aDescription
-        (value_index, was_added,) = self._addValueWithCheck(aModel)
-        self._contexts_by_value_index[value_index] = self._context
-        self._walkFromCurrent()
-        return self._contexts
+        def _addValueWithCheck(self, aValue):
+            identifier = id(aValue)
+            if identifier in self._value_indices_by_identifier:
+                was_added = False
+                value_index = self._value_indices_by_identifier[identifier]
+            else:
+                was_added = True
+                value_index = len(self._values)
+                self._values.append(aValue)
+                #reference = self.__class__._Reference()
+                #reference.reference_index = reference_index
+                #reference.value = aValue
+                self._value_indices_by_identifier[identifier] = value_index
+                self._values_by_value_index[value_index] = aValue
+            return (value_index, was_added,)
 
-    def _walkFromCurrent(self):
-        model = self._context.model
-        description = self._context.description
-        if model == description.undefinedValue:
-            return
-        if not description.visible:
-            return
-        description.acceptMagritte(self)
+        def _walkFromCurrent(self):
+            model = self._context.model
+            description = self._context.description
+            if model == description.undefinedValue:
+                return
+            if not description.visible:
+                return
+            description.acceptMagritte(self)
 
-    def visitElementDescription(self, description: MADescription):
-        context = self._context
-        model = context.model
-        value = MAModel.readUsingWrapper(model, description)
-        value_index = self._addValue(value)
-        context.value_index = value_index
-        context._dbg_value = value
+        def visitElementDescription(self, description: MADescription):
+            context = self._context
+            if self._doReadElementValues:
+                model = context.model
+                value = MAModel.readUsingWrapper(model, description)
+                value_index = self._addValue(value)
+                context.value_index = value_index
+                context._dbg_value = value
+            else:
+                context.value_index = -1
 
-    def visitContainer(self, description: MAContainer):
-        context = self._context
-        model = context.model
-        context.subcontexts = []
-        for subdescription in description:
-            #submodel = MAModel.readUsingWrapper(model, subdescription)
-            subcontext = self._createEmptyContext()
-            context.subcontexts.append(subcontext)
-            subcontext.model = model
-            subcontext.description = subdescription
-            self._walkFromCurrent()
+        def visitContainer(self, description: MAContainer):
+            context = self._context
+            model = context.model
+            context.subcontexts = []
+            for subdescription in description:
+                #submodel = MAModel.readUsingWrapper(model, subdescription)
+                subcontext = self._createEmptyContext()
+                context.subcontexts.append(subcontext)
+                subcontext.model = model
+                subcontext.description = subdescription
+                self._walkFromCurrent()
 
-    def visitToOneRelationDescription(self, description: MAReferenceDescription):
-        context = self._context
-        model = context.model
-        value = MAModel.readUsingWrapper(model, description)
-        (value_index, was_added,) = self._addValueWithCheck(value)
-        if was_added:
-            subcontext = self._createEmptyContext()
-            subcontext.model = value
-            subcontext.description = description.reference
-            self._contexts_by_value_index[value_index] = subcontext
-            self._walkFromCurrent()
-        else:
-            subcontext = self._contexts_by_value_index[value_index]
-            subcontext._dbg_ref_count += 1
-        context.subcontexts = [subcontext]
-
-    def visitToManyRelationDescription(self, description):
-        context = self._context
-        model = context.model
-        values = MAModel.readUsingWrapper(model, description)
-        context.subcontexts = []
-        for value in values:
+        def visitToOneRelationDescription(self, description: MAReferenceDescription):
+            context = self._context
+            model = context.model
+            value = MAModel.readUsingWrapper(model, description)
             (value_index, was_added,) = self._addValueWithCheck(value)
             if was_added:
                 subcontext = self._createEmptyContext()
@@ -158,7 +133,38 @@ class MAReferencedDataUntangler(MAVisitor):
             else:
                 subcontext = self._contexts_by_value_index[value_index]
                 subcontext._dbg_ref_count += 1
-            context.subcontexts.append(subcontext)
+            context.subcontexts = [subcontext]
+
+        def visitToManyRelationDescription(self, description):
+            context = self._context
+            model = context.model
+            values = MAModel.readUsingWrapper(model, description)
+            context.subcontexts = []
+            for value in values:
+                (value_index, was_added,) = self._addValueWithCheck(value)
+                if was_added:
+                    subcontext = self._createEmptyContext()
+                    subcontext.model = value
+                    subcontext.description = description.reference
+                    self._contexts_by_value_index[value_index] = subcontext
+                    self._walkFromCurrent()
+                else:
+                    subcontext = self._contexts_by_value_index[value_index]
+                    subcontext._dbg_ref_count += 1
+                context.subcontexts.append(subcontext)
+
+    def __init__(self):
+        self._walker = self.__class__._Walker()
+
+    def processModel(self, aModel: Any, aDescription: MADescription, doReadElementValues=False):
+        self._walker._clear()
+        self._walker._doReadElementValues = doReadElementValues
+        self._walker._context.model = aModel
+        self._walker._context.description = aDescription
+        (value_index, was_added,) = self._walker._addValueWithCheck(aModel)
+        self._walker._contexts_by_value_index[value_index] = self._walker._context
+        self._walker._walkFromCurrent()
+        return self._walker._contexts
 
 
 class MAReferencedDataJsonWriter:
@@ -226,15 +232,15 @@ if __name__ == "__main__":
     hostDescriptor = TestModelDescriptor.description_for("Host")
     testVisitor = MAReferencedDataUntangler()
     testVisitor.processModel(host, hostDescriptor)
-    testVisitor._dbg_print()
+    testVisitor._walker._dbg_print()
 
     port = host.ports[0]
     portDescriptor = TestModelDescriptor.description_for("Port")
     testVisitor.processModel(port, portDescriptor)
-    testVisitor._dbg_print()
+    testVisitor._walker._dbg_print()
 
     jsonWriter = MAReferencedDataJsonWriter()
-    jsonWriter.write_json(host, hostDescriptor)    j = jsonWriter.write_json(host, hostDescriptor)
+    j = jsonWriter.write_json(host, hostDescriptor)
     print(j)
 
     jsonReader = MAReferencedDataJsonReader()
