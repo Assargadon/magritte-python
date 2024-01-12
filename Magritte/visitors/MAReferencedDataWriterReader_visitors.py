c1270110704be21ba6d907737cab392f9093a2f7
Константин Калинин <konstantin.kalinin@wizn.tech>
2024-01-12 15:28:28 +0300

    Декомпозиция шаг 2

--- Magritte/visitors/MAReferencedDataWriterReader_visitors.py
+++ Magritte/visitors/MAReferencedDataWriterReader_visitors.py
@@ -23,39 +23,20 @@ class MADescriptorWalker:
 
         def __init__(self):
             self._contexts = None
+            self._contexts_by_source_id = None
             self._values = None
             self._values_by_value_index = None
             self._value_indices_by_identifier = None
             self._value_indices_by_context_index = None
-            self._contexts_by_value_index = None
             self._clear()
 
         def _clear(self):
             self._contexts = []
+            self._contexts_by_source_id = {}
             self._values = []
             self._values_by_value_index = {}
             self._value_indices_by_identifier = {}
             self._value_indices_by_context_index = {}
-            self._contexts_by_value_index = {}
-
-        def _dbg_print(self):
-            printed_contexts = set()
-            def printContext(sPrefix: str, aContext):
-                context_index = aContext.context_index
-                printed_contexts.add(context_index)
-                print(f'{sPrefix}Context {context_index}, {aContext.description.name}, referenced {aContext._dbg_ref_count} time(s):')
-                subPrefix = f'{sPrefix}  '
-                if aContext.subcontexts is None:
-                    print(f'{subPrefix}Value of {aContext.description.name}, {self._values[self._value_indices_by_context_index[aContext.context_index]]}')
-                else:
-                    for subcontext in aContext.subcontexts:
-                        subcontext_index = subcontext.context_index
-                        if subcontext_index in printed_contexts:
-                            print(f'{subPrefix}Context {subcontext_index}, {subcontext.description.name} // already printed')
-                        else:
-                            printContext(subPrefix, subcontext)
-
-            printContext('', self._contexts[0])
 
         def _createEmptyContext(self):
             context_index = len(self._contexts)
@@ -93,7 +74,10 @@ class MADescriptorWalker:
             description.acceptMagritte(self)
 
         def visitElementDescription(self, description: MADescription):
-            self.processElementDescription()
+            context = self._context
+            value = self.processElementDescriptionContext(context)
+            value_index = self._addValue(value)
+            self._value_indices_by_context_index[context.context_index] = value_index
 
         def visitContainer(self, description: MAContainer):
             context = self._context
@@ -107,75 +91,114 @@ class MADescriptorWalker:
 
         def visitToOneRelationDescription(self, description: MAReferenceDescription):
             context = self._context
-            model = context.source
-            value = MAModel.readUsingWrapper(model, description)
+            value, subsource = self.processToOneRelationContext(context, description)
             (value_index, was_added,) = self._addValueWithCheck(value)
             if was_added:
                 subcontext = self._createEmptyContext()
-                subcontext.source = value
+                subcontext.source = subsource
                 subcontext.description = description.reference
-                self._contexts_by_value_index[value_index] = subcontext
+                self._contexts_by_source_id[value_index] = subcontext
                 self._walkFromCurrent()
             else:
-                subcontext = self._contexts_by_value_index[value_index]
+                subcontext = self._contexts_by_source_id[value_index]
                 subcontext._dbg_ref_count += 1
             context.subcontexts = [subcontext]
 
         def visitToManyRelationDescription(self, description):
             context = self._context
-            model = context.source
-            values = MAModel.readUsingWrapper(model, description)
+            manyRelations = self.processToManyRelationContext(context, description)
             context.subcontexts = []
-            for value in values:
+            for relation in manyRelations:
+                (value, subsource,) = relation
                 (value_index, was_added,) = self._addValueWithCheck(value)
                 if was_added:
                     subcontext = self._createEmptyContext()
-                    subcontext.source = value
+                    subcontext.source = subsource
                     subcontext.description = description.reference
-                    self._contexts_by_value_index[value_index] = subcontext
+                    self._contexts_by_source_id[value_index] = subcontext
                     self._walkFromCurrent()
                 else:
-                    subcontext = self._contexts_by_value_index[value_index]
+                    subcontext = self._contexts_by_source_id[value_index]
                     subcontext._dbg_ref_count += 1
                 context.subcontexts.append(subcontext)
 
-        def processElementDescription(self):
-            pass
+        def processRootContext(self, context):
+            return None
+
+        def processElementDescriptionContext(self, context):
+            return None
+
+        def processToOneRelationContext(self, context, description):
+            return (None, None,)
+
+        def processToManyRelationContext(self, context, description):
+            return []
 
         def walkDescription(self, aSource: Any, aDescription: MADescription):
             self._clear()
             self._createEmptyContext()
             self._context.source = aSource
             self._context.description = aDescription
-            (value_index, was_added,) = self._addValueWithCheck(aSource)
-            self._contexts_by_value_index[value_index] = self._context
+            value = self.processRootContext(self._context)
+            (value_index, was_added,) = self._addValueWithCheck(value)
+            self._contexts_by_source_id[value_index] = self._context
             self._walkFromCurrent()
             return self._contexts
+
 
     class _ModelDumpWalkerVisitor(_WalkerVisitor):
         def __init__(self):
             super().__init__()
             self._doReadElementValues = None
 
-        def processElementDescription(self):
-            super().processElementDescription()
-            if self._doReadElementValues:
-                context = self._context
-                model = context.source
-                value = MAModel.readUsingWrapper(model, context.description)
-                value_index = self._addValue(value)
-                self._value_indices_by_context_index[context.context_index] = value_index
-                #context.value_index = value_index
-                #context._dbg_value = value
+        def _dbg_print(self):
+            printed_contexts = set()
+            def printContext(sPrefix: str, aContext):
+                context_index = aContext.context_index
+                printed_contexts.add(context_index)
+                print(f'{sPrefix}Context {context_index}, {aContext.description.name}, referenced {aContext._dbg_ref_count} time(s):')
+                subPrefix = f'{sPrefix}  '
+                if aContext.subcontexts is None:
+                    print(f'{subPrefix}Value of {aContext.description.name}, {self._values[self._value_indices_by_context_index[aContext.context_index]]}')
+                else:
+                    for subcontext in aContext.subcontexts:
+                        subcontext_index = subcontext.context_index
+                        if subcontext_index in printed_contexts:
+                            print(f'{subPrefix}Context {subcontext_index}, {subcontext.description.name} // already printed')
+                        else:
+                            printContext(subPrefix, subcontext)
 
-        def processModel(self, aModel: Any, aDescription: MADescription, doReadElementValues: bool):
+            printContext('', self._contexts[0])
+
+        def processRootContext(self, context):
+            value = context.source
+            return value
+
+        def processElementDescriptionContext(self, context):
+            if self._doReadElementValues:
+                value = MAModel.readUsingWrapper(context.source, context.description)
+                return value
+            else:
+                return None
+
+        def processToOneRelationContext(self, context, description):
+            model = context.source
+            value = MAModel.readUsingWrapper(model, description)
+            return (value, value,)
+
+        def processToManyRelationContext(self, context, description):
+            values = MAModel.readUsingWrapper(context.source, description)
+            manyRelations = [(value, value,) for value in values]
+            return manyRelations
+
+        def processModel(self, aModel: Any, aDescription: MADescription, doReadElementValues=False):
             self._doReadElementValues = doReadElementValues
             return super().walkDescription(aModel, aDescription)
 
 
-    def processModel(self, aModel: Any, aDescription: MADescription, doReadElementValues=False):
+    def processModel(self, aModel: Any, aDescription: MADescription):
         walker = self.__class__._ModelDumpWalkerVisitor()
-        return walker.processModel(aModel, aDescription, doReadElementValues)
+        return walker.processModel(aModel, aDescription)
 
 
 class MAReferencedDataJsonWriter:
