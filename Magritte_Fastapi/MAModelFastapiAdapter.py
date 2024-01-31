3636e64394d282e977b1b8113bd9656ee033f395
Константин Калинин <konstantin.kalinin@wizn.tech>
2024-01-31 15:28:49 +0300

    Выделен файл примера использования MAModelFastapiAdapter

--- Magritte_Fastapi/MAModelFastapiAdapter.py
+++ Magritte_Fastapi/MAModelFastapiAdapter.py
@@ -2,11 +2,8 @@
 from typing import ClassVar
 from Magritte.visitors.MAReferencedDataWriterReader_visitors import \
     MAReferencedDataHumanReadableSerializer, MAReferencedDataHumanReadableDeserializer
-from fastapi import FastAPI
 from pydantic import JsonValue
-from Magritte.descriptions.MAToManyRelationDescription_class import MAToManyRelationDescription
-from Magritte.descriptions.MAStringDescription_class import MAStringDescription
-from Magritte.accessors.MAIdentityAccessor_class import MAIdentityAccessor
+
 
 class MAModelFastapiAdapter:
     serializer: ClassVar = MAReferencedDataHumanReadableSerializer()
@@ -50,6 +47,7 @@ if __name__ == "__main__":
 
     from Magritte.model_for_tests.EnvironmentProvider_test import TestEnvironmentProvider
     from Magritte.model_for_tests.ModelDescriptor_test import TestModelDescriptor, Host, Port
+    from fastapi import FastAPI
     import uvicorn
 
 
@@ -77,25 +75,5 @@ if __name__ == "__main__":
     @MAModelFastapiAdapter.describe(request_descriptor=hostDescriptor, response_descriptor=portDescriptor)
     async def test(request: Host) -> Port:
         return request.ports[2]
-
-# ============= EXAMPLE ====================
-    @app.get("/hosts")
-    @MAModelFastapiAdapter.describe(request_descriptor=None, response_descriptor=MAToManyRelationDescription(reference=hostDescriptor, accessor=MAIdentityAccessor()))
-    async def get_hosts():
-        return provider.hosts
-
-    @app.post("/add_host")
-    @MAModelFastapiAdapter.describe(request_descriptor=hostDescriptor, response_descriptor=MAStringDescription(accessor=MAIdentityAccessor()))
-    async def add_host(host):
-        provider.hosts.append(host)
-        return "OK"
-
-    @app.get("/desc/host")
-    @MAModelFastapiAdapter.describe(request_descriptor=None, response_descriptor=hostDescriptor.magritteDescription())
-    async def host_desc():
-        return hostDescriptor
-
-# ============= /EXAMPLE ====================
-
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ Magritte_Fastapi/MAModelFastapiAdapter_example.py
@@ -0,0 +1,43 @@
+
+from fastapi import FastAPI
+from MAModelFastapiAdapter import MAModelFastapiAdapter
+from Magritte.descriptions.MAToManyRelationDescription_class import MAToManyRelationDescription
+from Magritte.descriptions.MAStringDescription_class import MAStringDescription
+from Magritte.accessors.MAIdentityAccessor_class import MAIdentityAccessor
+
+
+if __name__ == "__main__":
+
+    from Magritte.model_for_tests.EnvironmentProvider_test import TestEnvironmentProvider
+    from Magritte.model_for_tests.ModelDescriptor_test import TestModelDescriptor, Host
+    import uvicorn
+
+
+    provider = TestEnvironmentProvider()
+    hostDescriptor = TestModelDescriptor.description_for("Host")
+
+
+    app = FastAPI()
+
+
+# ============= EXAMPLE ====================
+    @app.get("/hosts")
+    @MAModelFastapiAdapter.describe(request_descriptor=None, response_descriptor=MAToManyRelationDescription(reference=hostDescriptor, accessor=MAIdentityAccessor()))
+    async def get_hosts():
+        return provider.hosts
+
+    @app.post("/add_host")
+    @MAModelFastapiAdapter.describe(request_descriptor=hostDescriptor, response_descriptor=MAStringDescription(accessor=MAIdentityAccessor()))
+    async def add_host(host):
+        provider.hosts.append(host)
+        return "OK"
+
+    @app.get("/desc/host")
+    @MAModelFastapiAdapter.describe(request_descriptor=None, response_descriptor=hostDescriptor.magritteDescription())
+    async def host_desc():
+        return hostDescriptor
+
+# ============= /EXAMPLE ====================
+
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
