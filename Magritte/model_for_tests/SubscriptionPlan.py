b3e91bb9601544fb839a143d166fa631d69a196e
Константин Калинин <konstantin.kalinin@wizn.tech>
2024-03-07 14:30:03 +0300

    Убран сложный конструктор SubscriptionPlan

--- Magritte/model_for_tests/SubscriptionPlan.py
+++ Magritte/model_for_tests/SubscriptionPlan.py
@@ -2,17 +2,25 @@ from Magritte.MAModel_class import MAModel
 
 class SubscriptionPlan(MAModel):
 
-    def __init__(self, name, price, description):
-        self.name = name
-        self.price = price
-        self.description = description
+    @classmethod
+    def subscriptionPlan(cls, name, price, description):
+        p = cls()
+        p.name = name
+        p.price = price
+        p.description = description
+        return p
+
+    def __init__(self):
+        self.name = None
+        self.price = None
+        self.description = None
 
     def __str__(self):
         return f"{self.name} [{self.price==0 and 'Free' or f'${self.price}/month'}]"
 
 SubscriptionPlan.entries = [
-    SubscriptionPlan("Community", 0, "Free plan for non-commercial use"),
-    SubscriptionPlan("Basic", 10, "Basic plan - good entry point for small teams"),
-    SubscriptionPlan("Pro", 25, "Pro plan"),
-    SubscriptionPlan("Enterprise", 40, "Enterprise plan for most demanding users"),
+    SubscriptionPlan.subscriptionPlan("Community", 0, "Free plan for non-commercial use"),
+    SubscriptionPlan.subscriptionPlan("Basic", 10, "Basic plan - good entry point for small teams"),
+    SubscriptionPlan.subscriptionPlan("Pro", 25, "Pro plan"),
+    SubscriptionPlan.subscriptionPlan("Enterprise", 40, "Enterprise plan for most demanding users"),
 ]
