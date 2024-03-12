883edc3d4509efcb34ed120ce4b6129fbdb30063
Константин Калинин <konstantin.kalinin@wizn.tech>
2024-03-12 22:04:46 +0300

    Тесты для SingleOptionDescription

--- Magritte/visitors/tests/MAReferencedDataWriterReader_visitors_test.py
+++ Magritte/visitors/tests/MAReferencedDataWriterReader_visitors_test.py
@@ -4,7 +4,7 @@ from json import dumps, loads
 from Magritte.visitors.MAReferencedDataWriterReader_visitors import MAReferencedDataHumanReadableSerializer, MAReferencedDataHumanReadableDeserializer
 
 from Magritte.model_for_tests.EnvironmentProvider_test import TestEnvironmentProvider
-from Magritte.model_for_tests.ModelDescriptor_test import TestModelDescriptor, Host, Port, Account
+from Magritte.model_for_tests.ModelDescriptor_test import TestModelDescriptor, Host, Port, Account, User, Organization, SubscriptionPlan
 
 
 class MAReferencedDataWriterVisitorTestBase(TestCase):
@@ -16,16 +16,25 @@ class MAReferencedDataWriterVisitorTestBase(TestCase):
         self.portDescription = TestModelDescriptor.description_for(Port.__name__)
         self.account = provider.accounts[1]
         self.accountDescription = TestModelDescriptor.description_for(Account.__name__)
+        self.user = provider.users[1];
+        self.userDescription = TestModelDescriptor.description_for(User.__name__)
 
-    def findDescriptionByProperty(self, prop):
-        getter = prop.fget
-        name = getter.__name__
-        class_name = getter.__qualname__.split(".")[0]
+    def findDescription(self, class_name, name):
         container = TestModelDescriptor.description_for(class_name)
         self.assertIsNotNone(container)
         descriptor = next(filter(lambda description: description.name == name, container.children), None)
         self.assertIsNotNone(descriptor)
         return descriptor
+
+    def findDescriptionByName(self, cls, name):
+        class_name = cls.__name__
+        return self.findDescription(class_name, name)
+
+    def findDescriptionByProperty(self, prop):
+        getter = prop.fget
+        name = getter.__name__
+        class_name = getter.__qualname__.split(".")[0]
+        return self.findDescription(class_name, name)
 
 
 class MAReferencedDataWriterVisitorTest(MAReferencedDataWriterVisitorTestBase):
@@ -79,6 +88,19 @@ class MAReferencedDataWriterVisitorTest(MAReferencedDataWriterVisitorTestBase):
         hostSerialized = self.serializer.serializeHumanReadable(self.host, self.hostDescription)
         hostFromJson = loads(hostSerialized)
         self.assertIsInstance(hostFromJson, dict, "MAContainerDescription in a serialized form should result in a json object")
+
+    def testSingleOptionDescriptionToElementDescription(self):
+        portStatusDescription = self.findDescriptionByProperty(Port.status)
+        portStatusDumped = self.serializer.dumpHumanReadable(self.port.status, portStatusDescription)
+        self.assertIsInstance(portStatusDumped, str, f"MASingleOptionDecription of MAStringDescription in a dumped form should result in a str, got {portStatusDumped}")
+        self.assertEqual(self.port.status, portStatusDumped, "MASingleOptionDecription of MAStringDescription in a dumped form should be equal to the source string")
+
+    def testSingleOptionDescriptionToContainerDescription(self):
+        userPlanDescription = self.findDescriptionByName(User, 'plan')
+        userPlanDumped = self.serializer.dumpHumanReadable(self.user.plan, userPlanDescription)
+        self.assertIsInstance(userPlanDumped, dict, f"MASingleOptionDecription of MAContainer in a dumped form should result in a dict, got {userPlanDumped}")
+        self.assertEqual(userPlanDumped['name'], self.user.plan.name, f"MASingleOptionDecription of MAContainer in a dumped form should have properties from the referenced object")
+        self.assertEqual(userPlanDumped['price'], self.user.plan.price, f"MASingleOptionDecription of MAContainer in a dumped form should have properties from the referenced object")
 
     def testIgnoreReadonly(self):
         portLabelDescription = self.findDescriptionByProperty(Port.label)
@@ -141,6 +163,13 @@ class MAReferencedDataReaderVisitorTest(MAReferencedDataWriterVisitorTestBase):
         portsDeserialized = self.deserializer.deserializeHumanReadable(portsJson, portsDescription)
         self.assertIsInstance(portsDeserialized, list, f"MAToManyRelationDescription in a deserialized form should result in a list, got {portsDeserialized}")
 
+    def testSingleOptionDescriptionToElementDescription(self):
+        portStatusDescription = self.findDescriptionByProperty(Port.status)
+        portStatusJson = dumps(self.port.status)
+        portStatusDeserialized = self.deserializer.deserializeHumanReadable(portStatusJson, portStatusDescription)
+        self.assertIsInstance(portStatusDeserialized, str, f"MASingleOptionDecription of MAStringDescription in a deserialized form should result in a str, got {portStatusDeserialized}")
+        self.assertEqual(self.port.status, portStatusDeserialized, "MASingleOptionDecription of MAStringDescription in a deserialized form should be equal to the source string")
+
 
 class MAReferencedDataWriterReaderVisitorPassthroughTest(MAReferencedDataWriterVisitorTestBase):
 
@@ -155,6 +184,7 @@ class MAReferencedDataWriterReaderVisitorPassthroughTest(MAReferencedDataWriterV
         self.assertIsInstance(dto_port, Port, f"Passed through port should result in Port instance, got {dto_port}")
         self.assertEqual(self.port.numofport, dto_port.numofport, f"Passed through port should have the same numofport {self.port.numofport}, got {dto_port.numofport}")
         self.assertEqual(self.port.host.ip, dto_port.host.ip, f"Passed through port should have the same ip of the host {self.port.host.ip}, got {dto_port.host.ip}")
+        self.assertEqual(self.port.status, dto_port.status, f"Passed through port should have the same status {self.port.status}, got {dto_port.status}")
         self.assertEqual(len(self.port.host.ports), len(dto_port.host.ports), f"Passed through port should have the same number of ports of the host {len(self.port.host.ports)}, got {len(dto_port.host.ports)}")
 
     def test_passthroughWithHost(self):
@@ -171,3 +201,13 @@ class MAReferencedDataWriterReaderVisitorPassthroughTest(MAReferencedDataWriterV
         self.assertIsInstance(dto_ports, list, f"Passed through ports should result in list instance, got {dto_ports}")
         self.assertEqual(len(self.host.ports), len(dto_ports), f"Passed through ports list should have the same length as for host {len(self.host.ports)}, got {len(dto_ports)}")
 
+    def test_passthroughWithUser(self):
+        serialized_str_user = self.serializer.serializeHumanReadable(self.user, self.userDescription)
+        dto_user = self.deserializer.deserializeHumanReadable(serialized_str_user, self.userDescription)
+        self.assertIsInstance(dto_user, User, f"Passed through host should result in Host instance, got {dto_user}")
+        self.assertIsInstance(dto_user.plan, SubscriptionPlan, f"Passed through user should have SubscriptionPlan instance in plan, got {dto_user.plan}")
+        self.assertIsInstance(dto_user.organization, Organization, f"Passed through user should have Organization instance in organization, got {dto_user.organization}")
+        self.assertEqual(self.user.regnum, dto_user.regnum, f"Passed through user should have the same regnum {self.user.regnum}, got {dto_user.regnum}")
+        self.assertEqual(self.user.dateofbirth, dto_user.dateofbirth, f"Passed through user should have the same dateofbirth {self.user.dateofbirth}, got {dto_user.dateofbirth}")
+        self.assertEqual(len(self.user.setofaccounts), len(dto_user.setofaccounts), f"Passed through user should have the same number of accounts {len(self.user.setofaccounts)}, got {len(dto_user.setofaccounts)}")
+        self.assertEqual(len(self.user.organization.listusers), len(dto_user.organization.listusers), f"Passed through user should have the same number of users of the organization {len(self.user.organization.listusers)}, got {len(dto_user.organization.listusers)}")
