696a8c559ba6c8c824566cd316b6fca41f5b6c51
Yuriy Mironenko <assargaddon@gmail.com>
2024-04-02 14:50:23 +0300

    FKeysMapper prototype
    toMany isnt supported yet

---
+++ MagritteSQLAlchemy/experiments/fkeysmapper.py
@@ -0,0 +1,57 @@
+from sqlalchemy import Table, Column, Integer, String, Date, Boolean, DateTime
+
+from Magritte.descriptions.MAContainer_class import MAContainer
+from Magritte.visitors.MAVisitor_class import MAVisitor
+from sqlalchemy.orm import registry as sa_registry
+from sqlalchemy.schema import ForeignKeyConstraint
+
+class FKeysMapper(MAVisitor):
+    def __init__(self):
+        self.table = None
+
+    def map(self, description: MAContainer, registered_tables: map):
+        self.registered_tables = registered_tables
+        self.my_table = self.registered_tables[description.sa_tableName]
+        self.visit(description)
+
+    # adds a foreign key to the source_table which points to the target_table with the property_name as part of the fields for the foreign key columns
+    def append_fkey(self, property_name: String, source_table: Table, target_table: Table):
+        # first, let's add all the columns to the source_table that are part of the foreign key
+        primary_keys_of_target = target_table.primary_key
+        if(primary_keys_of_target is None or len(primary_keys_of_target) == 0):
+            raise ValueError(f'Target table {target_table.name} does not have primary keys')
+
+        fkey_columns = []
+        for column in primary_keys_of_target:
+            fkey_column_name = f'{property_name}_{column.name}'
+            fkey_column = Column(fkey_column_name, column.type)
+            source_table.append_column(fkey_column)
+            fkey_columns.append(fkey_column)
+         
+        # now, let's add the foreign key constraint to the source_table)
+        source_table.append_constraint(
+            ForeignKeyConstraint(
+                columns = fkey_columns,
+                refcolumns = primary_keys_of_target
+            )
+        )
+
+
+    def visitDescription(self, description):
+        print(f'Description {description} not supported in FKeysMapper')
+
+    def visitContainer(self, description):
+        print(f'visitContainer {description.name}')
+        self.visitAll(description.children)
+
+
+    def visitToOneRelationDescription(self, description):
+        print(f'visitToOneRelationDescription {description.name}')
+        target_table = self.registered_tables[description.reference.sa_tableName]
+        self.append_fkey(description.name, self.my_table, target_table)
+
+
+#    def visitToManyRelationDescription(self, description):
+#        print(f'visitToManyRelationDescription {description.name}')
+#        target_table = self.registered_tables[description.reference.sa_tableName]
+#        self.append_fkey(description.name, target_table, self.my_table)
--- MagritteSQLAlchemy/experiments/registrator.py
+++ MagritteSQLAlchemy/experiments/registrator.py
@@ -2,40 +2,63 @@ from sqlalchemy import Table, Column, Integer
 
 from Magritte.descriptions.MAContainer_class import MAContainer
 from sqlalchemy.orm import registry as sa_registry
-from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import relationship
 
 from MagritteSQLAlchemy.experiments.fieldsmapper import FieldsMapper
+from MagritteSQLAlchemy.experiments.fkeysmapper import FKeysMapper
+
 from Magritte.descriptions.MAReferenceDescription_class import MAReferenceDescription
 from Magritte.descriptions.MASingleOptionDescription_class import MASingleOptionDescription
+from Magritte.descriptions.MAToOneRelationDescription_class import MAToOneRelationDescription
 
+
+def add_missing_primary_keys(table: Table) -> Table:
+    if not any([col.primary_key for col in table.c]):
+        table.append_column(Column("id", Integer, primary_key=True))
+    return table
 
 def register(*descriptors: MAContainer, registry: sa_registry = None) -> sa_registry:
 
     if not registry:
         registry = sa_registry()
+
+    registered_tables = {}
     fields_mapper = FieldsMapper()
-
     for descriptor in descriptors:
-        print(f' ================= > Registering {descriptor.name} ...')
 
+        # table stub
         table = Table(
             descriptor.sa_tableName,
             registry.metadata,
             )
 
-        table.append_column(Column("id", Integer, primary_key=True))
-
+        # map scalar fields
         fields_mapper.map(descriptor, table)
 
+        # add missing primary keys
+        table = add_missing_primary_keys(table)
+
+        registered_tables[descriptor.sa_tableName] = table
         print(table.c)
 
-        print(" ============================================================= ")
+    fkeys_mapper = FKeysMapper()
+    for descriptor in descriptors:
+        # add foreign keys
+        fkeys_mapper.map(descriptor, registered_tables) 
+
+    for descriptor in descriptors:
+        print(f' ================= > Registering {descriptor.name} ...')
 
         properties_to_map = {}
+        table = registered_tables[descriptor.sa_tableName]
         for desc in filter(lambda x: x.sa_storable, descriptor.children):
             if not isinstance(desc, MAReferenceDescription) or (isinstance(desc, MASingleOptionDescription) and not isinstance(desc.reference, MAContainer)):
                 print(f' Mapping scalar attribute = {desc.sa_attrName}')
                 properties_to_map[desc.sa_attrName] = table.c[desc.sa_fieldName]
+            if isinstance(desc, MAToOneRelationDescription) and isinstance(desc.reference, MAContainer):
+                print(f' Mapping REFERENCE attribute = {desc.sa_attrName}')
+                table = registered_tables[descriptor.sa_tableName]
+                properties_to_map[desc.sa_attrName] = relationship(desc.reference.kind)
                 
         registry.map_imperatively(
             descriptor.kind,
