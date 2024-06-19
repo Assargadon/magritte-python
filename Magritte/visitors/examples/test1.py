3527963584a3b207e3dbc0eb59bc8cfc487f3292
Val <valerii.filin@gmail.com>
2024-06-19 18:11:55 +0300

    ! Isolated conflict !
    A) SQLAlchemy instrumentation: copies list upon assignment.
    B) JSONDeserializer: assigns empty list to the model, then modifies the original copy of the list.
    Options:
    A) Modify the order of operations within JSON deserializer.
    B) Tune SQLAlchemy to not copy lists upon assignment (if there is such option). Might "contaminate" external objects (lists) with unnecessary instrumentation.

--- Magritte/visitors/examples/test1.py
+++ Magritte/visitors/examples/test1.py
@@ -13,18 +13,11 @@ from Magritte.visitors.MAReferencedDataWriterReader_visitors import (
 from MagritteSQLAlchemy.imperative import registrator
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
-
-
-class Host:
-    def __init__(self):
-        self.ip = None
-        self.ports = []
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.ERROR)
 
 
 class Port:
     def __init__(self):
-        self.host = None
         self.numofport = None
         self.features = []
 
@@ -35,31 +28,11 @@ class Feature:
         self.value = None
 
 
-host_desc = MAContainer(kind=Host, name="Host")
 port_desc = MAContainer(kind=Port, name="Port")
 feature_desc = MAContainer(kind=Feature, name="Feature")
 
-host_desc.setChildren([
-    MAStringDescription(name="ip", accessor=MAAttrAccessor("ip"), required=True),
-    MAToManyRelationDescription(
-        name="ports",
-        accessor=MAAttrAccessor("ports"),
-        classes=[Port],
-        reference=port_desc
-    )
-])
-
-host_ports_desc = host_desc[1]
-
 port_desc.setChildren([
     MAIntDescription(name="numofport", accessor=MAAttrAccessor("numofport"), required=True),
-    MAToOneRelationDescription(
-        name="host",
-        accessor=MAAttrAccessor("host"),
-        required=True,
-        classes=[Host],
-        reference=host_desc
-    ),
     MAToManyRelationDescription(
         name="features",
         accessor=MAAttrAccessor("features"),
@@ -68,43 +41,45 @@ port_desc.setChildren([
     )
 ])
 
-port_features_desc = port_desc[2]
-
 feature_desc.setChildren([
     MAStringDescription(name="name", accessor=MAAttrAccessor("name"), required=True),
     MAStringDescription(name="value", accessor=MAAttrAccessor("value"), required=True)
 ])
 
-descriptions = [host_desc, port_desc, feature_desc]
-
-registry = registrator.register(*descriptions)
-
+descriptions = [port_desc, feature_desc]
 
 if __name__ == '__main__':
 
-    host = Host()
     port = Port()
     feature = Feature()
 
-    host.ip = "192.168.0.3"
     port.numofport = 81
 
     feature.name = "feature_0"
     feature.value = "value_0"
 
-    port.features.append(feature)
+    features = []
+    logger.error(f"id(features): {hex(id(features))}, features = {features}")
+    port.features = features
+    logger.error(f"id(port.features): {hex(id(port.features))}, port.features = {port.features}")
+    features.append(feature)
+    logger.error(f"port.features[0].name: {port.features[0].name}")
 
-    # when ORM mapping is performed one of the following two lines should be commented out (due to backpopulates)
-    port.host = host
-    # host.ports.append(port)
+    # ============================================================================================
+    logger.error(f" Enabling ORM mapping ")
+    registry = registrator.register(*descriptions)
 
-    serialized_str_host = MAReferencedDataHumanReadableSerializer().serializeHumanReadable(host, host_desc)
-    logger.debug(f"Serialized host: {serialized_str_host}")
-    dto_host = MAReferencedDataHumanReadableDeserializer().deserializeHumanReadable(serialized_str_host, host_desc)
-    logger.debug(f"isinstance(dto_host, Host): {isinstance(dto_host, Host)}")
-    logger.debug(f"host.ip: {host.ip}")
-    logger.debug(f"dto_host.ip: {dto_host.ip}")
-    logger.debug(f"host.ports[0].numofport: {host.ports[0].numofport}")
-    logger.debug(f"dto_host.ports[0].numofport: {dto_host.ports[0].numofport}")
-    logger.debug(f"host.ports[0].features[0].name: {host.ports[0].features[0].name}")
-    logger.debug(f"dto_host.ports[0].features[0].name: {dto_host.ports[0].features[0].name}") # fails here
+    port = Port()
+    feature = Feature()
+
+    port.numofport = 81
+
+    feature.name = "feature_0"
+    feature.value = "value_0"
+
+    features = []
+    logger.error(f"id(features): {hex(id(features))}, features = {features}")
+    port.features = features
+    logger.error(f"id(port.features): {hex(id(port.features))}, port.features = {port.features}")
+    features.append(feature)
+    logger.error(f"port.features[0].name: {port.features[0].name}")
