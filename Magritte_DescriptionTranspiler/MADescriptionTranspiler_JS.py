ea69376fdae4f989d615e16622109955aa1d333d
Константин Калинин <konstantin.kalinin@wizn.tech>
2024-07-09 16:15:45 +0300

    Правки MADescriptionTranspiler_JS (динамическое вычисление импортов, возможность базовой настройки транспилируемых полей дескрипторов)

--- Magritte_DescriptionTranspiler/MADescriptionTranspiler_JS.py
+++ Magritte_DescriptionTranspiler/MADescriptionTranspiler_JS.py
@@ -1,5 +1,5 @@
 
-from typing import Any
+from typing import Any, Iterable
 import json
 import re
 from unicodedata import normalize
@@ -13,10 +13,10 @@ class MADescriptionTranspiler_JS:
 
     class _MADescriptorTranspiler_JS_Visitor(MAVisitor):
         @staticmethod
-        def variableNameByDescriptionName(description_name: str, existing_mapping: dict) -> str:
+        def variableNameByDescriptionName(description_name: str, description_names_mapping_to_js: dict) -> str:
             # Look for existing identifier
-            if description_name in existing_mapping:
-                return existing_mapping[description_name]
+            if description_name in description_names_mapping_to_js:
+                return description_names_mapping_to_js[description_name]
             # Normalize the string to form a valid Unicode identifier
             normalized_string = normalize('NFKD', description_name).encode('ascii', 'ignore').decode('ascii')
             # Replace non-alphanumeric characters and spaces with underscores
@@ -28,8 +28,13 @@ class MADescriptionTranspiler_JS:
                 valid_identifier = '_' + valid_identifier
             # Add "Desc" suffix
             valid_identifier = f'{valid_identifier}Desc'
+            # Check there is no already such string (because we lost some information while normalizing).
+            # If yes, prepend some underscores.
+            existing_identifiers = description_names_mapping_to_js.values()
+            while valid_identifier in existing_identifiers:
+                valid_identifier = f'_{valid_identifier}'
             # Return the valid identifier
-            existing_mapping[description_name] = valid_identifier
+            description_names_mapping_to_js[description_name] = valid_identifier
             return valid_identifier
 
         @staticmethod
@@ -48,13 +53,24 @@ class MADescriptionTranspiler_JS:
             return json_lines
 
         def __init__(self):
-            self._existing_mapping = None
+            self._descriptionFields = None
+            self._description_names_mapping_to_js = None
+            self._description_classes_to_import = None
             self._transpiled_lines = None
             self._reference_description_names = None
             self._options_dict = None
 
-        def transpile(self, anObject: MADescription, padding: int, existing_mapping: dict) -> tuple[list[str], set[str]]:
-            self._existing_mapping = existing_mapping
+        def transpile(
+                self,
+                anObject: MADescription,
+                descriptionFields: Iterable[str],
+                padding: int,
+                description_names_mapping_to_js: dict,
+                description_classes_to_import: set[str],
+        ) -> tuple[list[str], set[str]]:
+            self._descriptionFields = descriptionFields
+            self._description_names_mapping_to_js = description_names_mapping_to_js
+            self._description_classes_to_import = description_classes_to_import
             self._transpiled_lines = []
             self._reference_description_names = set()
             self._options_dict = {}
@@ -66,30 +82,24 @@ class MADescriptionTranspiler_JS:
             return self._transpiled_lines, self._reference_description_names
 
         def _createDescriptionWithOptions(self, anObject, options_dict):
+            description_class_name = anObject.type
+            self._description_classes_to_import.add(description_class_name)
             json_lines = self.dictToJsonLines(options_dict)
-            json_lines[0] = f'new {anObject.type}({json_lines[0]}'
+            json_lines[0] = f'new {description_class_name}({json_lines[0]}'
             json_lines[-1] = f'{json_lines[-1]})'
             self._transpiled_lines.extend(json_lines)
 
         def visitDescription(self, anObject: MADescription):
             super().visitDescription(anObject)
-            self._options_dict.update({
-                'name': self.valueToJsonString(anObject.name),
-                'group': self.valueToJsonString(anObject.group),
-                'label': self.valueToJsonString(anObject.label),
-                'comment': self.valueToJsonString(anObject.comment),
-                'required': self.valueToJsonString(anObject.required),
-                'priority': self.valueToJsonString(anObject.priority),
-                'visible': self.valueToJsonString(anObject.visible),
-                'readOnly': self.valueToJsonString(anObject.readOnly),
-                'undefinedValue': self.valueToJsonString(anObject.undefinedValue),
-            })
+            for fieldName in self._descriptionFields:
+                value = getattr(anObject, fieldName)
+                self._options_dict[fieldName] = self.valueToJsonString(value)
 
         def visitToOneRelationDescription(self, anObject: MAReferenceDescription):
             super().visitReferenceDescription(anObject)
             reference_name = anObject.reference.name
             self._options_dict.update({
-                'reference': self.variableNameByDescriptionName(reference_name, self._existing_mapping),
+                'reference': self.variableNameByDescriptionName(reference_name, self._description_names_mapping_to_js),
             })
             self._reference_description_names.add(reference_name)
 
@@ -97,7 +107,7 @@ class MADescriptionTranspiler_JS:
             super().visitReferenceDescription(anObject)
             reference_name = anObject.reference.name
             self._options_dict.update({
-                'reference': self.variableNameByDescriptionName(reference_name, self._existing_mapping),
+                'reference': self.variableNameByDescriptionName(reference_name, self._description_names_mapping_to_js),
             })
             self._reference_description_names.add(reference_name)
 
@@ -109,6 +119,17 @@ class MADescriptionTranspiler_JS:
         description_names_whitelist=None,
         generated_js_class_name='TranspiledDescriptors',
         magritte_js_import_prefix='magritte-js/Magritte/',
+        description_field_names=(
+            'name',
+            'group',
+            'label',
+            'comment',
+            'required',
+            'priority',
+            'visible',
+            'readOnly',
+            'undefinedValue',
+        )
     ) -> str:
         descriptor_instantiate_lines = []
         descriptor_initialize_lines = []
@@ -118,6 +139,7 @@ class MADescriptionTranspiler_JS:
         # Maintain a list of descriptions that are available and which are to be transpiled
         all_descriptions = descriptors.all_descriptions
         description_names_processed = set()
+        description_classes_to_import = set()
         description_names_mapping_to_js = dict()
         if description_names_whitelist is None:
             description_names_to_process = set([description.name for description in descriptors.all_descriptions])
@@ -145,17 +167,27 @@ class MADescriptionTranspiler_JS:
                     raise TypeError(f'{cls.__name__} can transpile only MAContainer descriptions')
 
                 # Get unique name for variable
-                js_variable_name = MADescriptionTranspiler_JS._MADescriptorTranspiler_JS_Visitor.variableNameByDescriptionName(description.name, description_names_mapping_to_js)
+                js_variable_name = MADescriptionTranspiler_JS._MADescriptorTranspiler_JS_Visitor.variableNameByDescriptionName(
+                    description.name, description_names_mapping_to_js
+                )
 
                 # Transpile to JS
-                descriptor_instantiate_lines.append(f'const {js_variable_name} = new MAContainer();')
+                container_class_name = 'MAContainer'
+                description_classes_to_import.add(container_class_name)
+                descriptor_instantiate_lines.append(f'const {js_variable_name} = new {container_class_name}();')
                 descriptor_initialize_lines.append('')
                 descriptor_initialize_lines.append(f'{js_variable_name}.name = {json.dumps(container.name)};')
                 descriptor_initialize_lines.append(f'{js_variable_name}.label = {json.dumps(container.label)};')
                 descriptor_initialize_lines.append(f'{js_variable_name}.group = {json.dumps(container.group)};')
                 descriptor_initialize_lines.append(f'{js_variable_name}.setChildren(')
                 for child_description in container.children:
-                    transpiled_lines, reference_description_names = transpiler.transpile(child_description, 4, description_names_mapping_to_js)
+                    transpiled_lines, reference_description_names = transpiler.transpile(
+                        child_description,
+                        description_field_names,
+                        4,
+                        description_names_mapping_to_js,
+                        description_classes_to_import,
+                    )
                     transpiled_lines[-1] = f'{transpiled_lines[-1]},'
                     descriptor_initialize_lines.extend(transpiled_lines)
                     new_description_names_to_process.update(reference_description_names)
@@ -180,15 +212,13 @@ class MADescriptionTranspiler_JS:
 
         # Add header
         js_lines = []
-        js_lines.append(f"import {{ MAContainer }} from '{magritte_js_import_prefix}descriptions/MAContainer.js';")
-        js_lines.append(f"import {{ MAToOneRelationDescription }} from '{magritte_js_import_prefix}descriptions/MAToOneRelationDescription.js';")
-        js_lines.append(f"import {{ MAToManyRelationDescription }} from '{magritte_js_import_prefix}descriptions/MAToManyRelationDescription.js';")
-        js_lines.append(f"import {{ MAStringDescription }} from '{magritte_js_import_prefix}descriptions/MAStringDescription.js';")
-        js_lines.append(f"import {{ MAIntDescription }} from '{magritte_js_import_prefix}descriptions/MAIntDescription.js';")
-        js_lines.append(f"import {{ MAElementDescription }} from '{magritte_js_import_prefix}descriptions/MAElementDescription.js';")
-        js_lines.append(f"import {{ MABooleanDescription }} from '{magritte_js_import_prefix}descriptions/MABooleanDescription.js';")
-        js_lines.append(f"import {{ MAPriorityContainer }} from '{magritte_js_import_prefix}descriptions/MAPriorityContainer.js';")
+
+        # Add description imports
+        for descriptionClass in sorted(description_classes_to_import):
+            js_lines.append(f"import {{ {descriptionClass} }} from '{magritte_js_import_prefix}descriptions/{descriptionClass}.js';")
         js_lines.append("")
+
+        # Export provider class as default export
         js_lines.append(f"export default class {generated_js_class_name} {{")
         js_lines.append("    constructor () {")
         js_lines.append("        this.descriptions_by_model_type = new Map();")
