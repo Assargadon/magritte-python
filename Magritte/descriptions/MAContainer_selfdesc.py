e22ef9e955af56fd6944f8c29a0a58a05ec6fa8c
Val <valerii.filin@gmail.com>
2024-10-07 13:34:29 +0300

    SelfDesc update for MAContainer

--- Magritte/descriptions/MAContainer_selfdesc.py
+++ Magritte/descriptions/MAContainer_selfdesc.py
@@ -1,8 +1,11 @@
 from sys import intern
 
 from Magritte.accessors.MAAttrAccessor_class import MAAttrAccessor
+from Magritte.descriptions.MAContainer_class import MAContainer
 from Magritte.descriptions.MAToManyRelationDescription_class import MAToManyRelationDescription
 from Magritte.descriptions.MAElementDescription_class import MAElementDescription
+from Magritte.descriptions.MAToOneRelationDescription_class import MAToOneRelationDescription
+
 
 def magritteDescription(self, parentDescription):
     desc = parentDescription
@@ -16,5 +19,17 @@ def magritteDescription(self, parentDescription):
         reference = MAElementDescription().magritteDescription(),
         accessor = MAAttrAccessor('children')
     )
+
+    ancestor_desc = MAToOneRelationDescription(
+        name=intern('ancestor'),
+        label="Ancestor",
+        priority=300,
+        default=None,
+        classes=[MAContainer],
+        reference = desc,
+        accessor = MAAttrAccessor('ancestor')
+    )
+
+    desc += ancestor_desc
     
     return desc
