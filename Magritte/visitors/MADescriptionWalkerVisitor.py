7f6ef6a380e928950cd26a626ae09919bc656685
Val <valerii.filin@gmail.com>
2025-02-13 13:26:21 +0400

    Walker visitors fine tuning

--- Magritte/visitors/MADescriptionWalkerVisitor.py
+++ Magritte/visitors/MADescriptionWalkerVisitor.py
@@ -1,3 +1,8 @@
+from copy import copy
+
+from Magritte.accessors.MAIdentityAccessor_class import MAIdentityAccessor
+from Magritte.descriptions.MADescription_class import MADescription
+from Magritte.descriptions.MASingleOptionDescription_class import MASingleOptionDescription
 from Magritte.visitors.MAVisitor_class import MAVisitor
 from Magritte.descriptions.MAContainer_class import MAContainer
 from Magritte.descriptions.MAElementDescription_class import MAElementDescription
@@ -13,9 +18,16 @@ class MADescriptionWalkerVisitor(MAVisitor):
         self.model_stack = []  # Use stack for tracking the current model
         self.model = None
 
+    def reset(self):
+        self.visited_objects.clear()
+        self.model_stack.clear()
+        self.model = None
+
     def walkDescription(self, model, description):
         """Main method for traversing the description."""
-        if model is None or description is None:
+        if description is None:
+            raise ValueError("Description cannot be None")
+        if model == description.undefinedValue:
             return
 
         model_id = id(model)
@@ -27,7 +39,7 @@ class MADescriptionWalkerVisitor(MAVisitor):
         self.visit(description)
         self.model_stack.pop()
 
-    def visit(self, description: MAElementDescription):
+    def visit(self, description: MADescription):
         """Visit the description."""
         self.model = self.model_stack[-1]
         super().visit(description)
@@ -39,13 +51,24 @@ class MADescriptionWalkerVisitor(MAVisitor):
     def visitToOneRelationDescription(self, description: MAToOneRelationDescription):
         """Process the "one-to-one" relation."""
         related_obj = MAModel.readUsingWrapper(self.model, description)
-        self.walkDescription(related_obj, description.reference)
+        if related_obj is not None:
+            self.walkDescription(related_obj, description.reference)
 
     def visitToManyRelationDescription(self, description: MAToManyRelationDescription):
         """Process the "one-to-many" relation."""
         related_objs = MAModel.readUsingWrapper(self.model, description)
-        for obj in related_objs:
-            self.walkDescription(obj, description.reference)
+        if related_objs is not None:
+            for obj in related_objs:
+                self.walkDescription(obj, description.reference)
+
+    def visitSingleOptionDescription(self, description: MASingleOptionDescription):
+        reference = description.reference
+        if isinstance(reference, MAContainer):
+            self.visitToOneRelationDescription(description)
+        elif isinstance(reference, MAElementDescription) and not isinstance(reference, MAToOneRelationDescription):
+            self.visitElementDescription(description)
+        else:
+            raise TypeError(f"Unsupported reference type in SingleOptionDescription: {type(reference)}")
 
     def visitElementDescription(self, description: MAElementDescription):
         """Process scalar values."""
@@ -55,37 +78,55 @@ class MAReferencedDataPrinter(MADescriptionWalkerVisitor):
     def __init__(self):
         super().__init__()
         self._indent = '= '
-        self._prefix = ''
+        self._level_prefix = ''
+        self._elem_prefix = '- '
+        self._c_prefix = ''
+        self._e_prefix = ''
 
-    def visit(self, description: MAElementDescription):
-        self._prefix = self._indent * len(self.model_stack)
+    def print(self, model, description):
+        self.reset()
+        print("======================================================")
+        self.walkDescription(model, description)
+        print("======================================================")
+
+    def visit(self, description: MADescription):
+        self._level_prefix = self._indent * len(self.model_stack)
+        self._c_prefix = self._level_prefix
+        self._e_prefix = self._level_prefix + self._elem_prefix
         super().visit(description)
 
     def visitContainer(self, description: MAContainer):
-        print(f"{self._prefix}Object described by {description.name} of kind {description.kind}: {self.model}")
+        print(f"{self._c_prefix}Object described by {description.name} of kind {description.kind}: {self.model}")
         super().visitContainer(description)
 
     def visitToOneRelationDescription(self, description: MAToOneRelationDescription):
-        print(f"{self._prefix}One-to-One relation: {description.name} of kind {description.kind}")
+        print(f"{self._e_prefix}{description.__class__.__name__}: {description.name}")
         super().visitToOneRelationDescription(description)
 
     def visitToManyRelationDescription(self, description: MAToManyRelationDescription):
-        print(f"{self._prefix}One-to-Many relation: {description.name} of kind {description.kind}")
+        print(f"{self._e_prefix}{description.__class__.__name__}: {description.name}")
         super().visitToManyRelationDescription(description)
 
     def visitElementDescription(self, description: MAElementDescription):
         value = MAModel.readUsingWrapper(self.model, description)
-        print(f"{self._prefix}- Element: {description.name} of kind {description.kind}: {value}")
+        print(f"{self._e_prefix}{description.__class__.__name__}: {description.name} of kind {description.kind}: {value}")
 
 # Test examples
 if __name__ == "__main__":
-    from Magritte.model_for_tests import Host
+    from Magritte.model_for_tests import Host, User, Organization
     from Magritte.model_for_tests.ModelDescriptor_test import TestModelDescriptorProvider
 
     desc_provider = TestModelDescriptorProvider()
+    org = Organization.random_organization()
     host = Host.random_host()
+    user = User.random_user(organization=org)
     host_desc = desc_provider.description_for("Host")
     port_desc = desc_provider.description_for("Port")
+    host_ports_desc = copy(host_desc['ports'])
+    host_ports_desc.accessor = MAIdentityAccessor()
+    user_desc = desc_provider.description_for("User")
     printer = MAReferencedDataPrinter()
-    # printer.walkDescription(host, host_desc)
-    printer.walkDescription(host.ports[0], port_desc)
+    printer.print(host, host_desc)
+    printer.print(host.ports[0], port_desc)
+    printer.print(host.ports, host_ports_desc)
+    printer.print(user, user_desc)
